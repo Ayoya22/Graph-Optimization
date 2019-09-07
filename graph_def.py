1caaf64d067e2501310b77cc302e741e7682c5ec
ayoya22 <djoukamsimplice@gmail.com>
2019-09-07 11:58:44 +0100

    saving image
    Signed-off-by: ayoya22 <djoukamsimplice@gmail.com>

--- graph_def.py
+++ graph_def.py
@@ -287,5 +287,32 @@ nx.draw_networkx_edge_labels(g_cpp, pos=node_positions, edge_labels=edge_labels,
 plt.axis('off')
 plt.show()
 
+visit_colors = {1:'black', 2:'red'}
+edge_cnter = {}
+g_i_edge_colors = []
+for i, e in enumerate(euler_circuit, start=1):
 
+    edge = frozenset([e[0], e[1]])
+    if edge in edge_cnter:
+        edge_cnter[edge] += 1
+    else:
+        edge_cnter[edge] = 1
+
+    # Full graph (faded in background)
+    nx.draw_networkx(g_cpp, pos=node_positions, node_size=6, node_color='gray', with_labels=False, alpha=0.07)
+
+    # Edges walked as of iteration i
+    euler_circuit_i = copy.deepcopy(euler_circuit[0:i])
+    for i in range(len(euler_circuit_i)):
+        edge_i = frozenset([euler_circuit_i[i][0], euler_circuit_i[i][1]])
+        euler_circuit_i[i][2]['visits_i'] = edge_cnter[edge_i]
+    g_i = nx.Graph(euler_circuit_i)
+    g_i_edge_colors = [visit_colors[e[2]['visits_i']] for e in g_i.edges(data=True)]
+
+    nx.draw_networkx_nodes(g_i, pos=node_positions, node_size=6, alpha=0.6, node_color='lightgray', with_labels=False, linewidths=0.1)
+    nx.draw_networkx_edges(g_i, pos=node_positions, edge_color=g_i_edge_colors, alpha=0.8)
+
+    plt.axis('off')
+    plt.savefig('fig/png/img{}.png'.format(i), dpi=120, bbox_inches='tight')
+    plt.close()
 
