fdb0100c8bc625d3f23bb39c14a2f48a54e5f330
ayoya22 <djoukamsimplice@gmail.com>
2019-09-07 08:24:40 +0100

    made some debugging.
    Signed-off-by: ayoya22 <djoukamsimplice@gmail.com>

--- graph_def.py
+++ graph_def.py
@@ -24,17 +24,33 @@ print(elrow[2:].to_dict())  # edge attribute dict
 
 # Adding node attributes
 for i, nlrow in nodelist.iterrows():
-    g.node[nlrow['id']]=nlrow[1:].to_dict()
+    g.node[nlrow['id']].update(nlrow[1:].to_dict())
 
 # Node list example
 print(nlrow)
 
 # Preview first 5 edges
-g.edges(data=True)[0:5]
+list(g.edges(data=True))[0:5]
 # Preview first 5 nodes
-g.nodes(data=True)[0:5]
+list(g.nodes(data=True))[0:5]
 
 # Print out some summary statistics before visualizing the graph.
 print('# of edges: {}'.format(g.number_of_edges()))
 print('# of nodes: {}'.format(g.number_of_nodes()))
 
+# Define node positions data structure (dict) for plotting
+node_positions = {node[0]: (node[1]['X'], -node[1]['Y']) for node in g.nodes(data=True)}
+
+# Preview of node_positions with a bit of hack (there is no head/slice method for dictionaries).
+dict(list(node_positions.items())[0:5])
+
+# Define data structure (list) of edge colors for plotting
+edge_colors = [e[2]['color'] for e in g.edges(data=True)]
+
+# Preview first 10
+edge_colors[0:10]
+
+plt.figure(figsize=(8, 6))
+nx.draw(g, pos=node_positions, edge_color=edge_colors, node_size=10, node_color='black')
+plt.title('Graph Representation of a simple program', size=15)
+plt.show()
