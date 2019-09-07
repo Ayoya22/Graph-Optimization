a91465565215538b0ea704f0b1829b1f6e8b0eaf
ayoya22 <djoukamsimplice@gmail.com>
2019-09-07 03:42:13 +0100

    Created empty graph and used the `edgelist` and `nodelist` to do so.
    Signed-off-by: ayoya22 <djoukamsimplice@gmail.com>

--- graph_def.py
+++ graph_def.py
@@ -8,3 +8,17 @@ nodelist = pd.read_csv('https://gist.githubusercontent.com/brooksandrew/f989e10a
 edgelist = pd.read_csv('https://gist.githubusercontent.com/brooksandrew/e570c38bcc72a8d102422f2af836513b/raw/89c76b2563dbc0e88384719a35cba0dfc04cd522/edgelist_sleeping_giant.csv')
 edgelist.head(10)
 nodelist.head(5)
+
+# Create empty graph
+g = nx.Graph()
+
+# Add edges and edge attributes
+for i, elrow in edgelist.iterrows():
+    g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
+
+# To illustrate what happened in the above iteration let me just print the values from the last row in the edge list
+# that got added to graph g:
+print(elrow[0])  # node1
+print(elrow[1])  # node2
+print(elrow[2:].to_dict())  # edge attribute dict
+
