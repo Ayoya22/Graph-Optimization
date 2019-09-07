7c8fcbe697b92b1b465a75acf7237914cadfddb3
ayoya22 <djoukamsimplice@gmail.com>
2019-09-07 09:42:16 +0100

    made some debugging again to fix the program.
    Signed-off-by: ayoya22 <djoukamsimplice@gmail.com>

--- graph_def.py
+++ graph_def.py
@@ -14,7 +14,7 @@ g = nx.Graph()
 
 # Add edges and edge attributes
 for i, elrow in edgelist.iterrows():
-    g.add_edge(elrow[0], elrow[1], attr_dict=elrow[2:].to_dict())
+    g.add_edge(elrow[0], elrow[1], **elrow[2:].to_dict())
 
 # To illustrate what happened in the above iteration let me just print the values from the last row in the edge list
 # that got added to graph g:
@@ -24,7 +24,7 @@ print(elrow[2:].to_dict())  # edge attribute dict
 
 # Adding node attributes
 for i, nlrow in nodelist.iterrows():
-    g.node[nlrow['id']].update(nlrow[1:].to_dict())
+    nx.set_node_attributes(g, {nlrow['id']:  nlrow[1:].to_dict()})
 
 # Node list example
 print(nlrow)
@@ -32,20 +32,20 @@ print(nlrow)
 # Preview first 5 edges
 list(g.edges(data=True))[0:5]
 # Preview first 5 nodes
-list(g.nodes(data=True))[0:5]
+list(g.nodes(data=True))[0:10]
 
 # Print out some summary statistics before visualizing the graph.
 print('# of edges: {}'.format(g.number_of_edges()))
 print('# of nodes: {}'.format(g.number_of_nodes()))
 
 # Define node positions data structure (dict) for plotting
-node_positions = {node[0]: (node[1]['X'], -node[1]['Y']) for node in g.nodes(data=True)}
+node_positions = {node[0]: (node[1]["X"], -node[1]["Y"]) for node in g.nodes(data=True)}
 
 # Preview of node_positions with a bit of hack (there is no head/slice method for dictionaries).
 dict(list(node_positions.items())[0:5])
 
 # Define data structure (list) of edge colors for plotting
-edge_colors = [e[2]['color'] for e in g.edges(data=True)]
+edge_colors = [e[2]['color'] for e in list(g.edges(data=True))]
 
 # Preview first 10
 edge_colors[0:10]
@@ -54,3 +54,5 @@ plt.figure(figsize=(8, 6))
 nx.draw(g, pos=node_positions, edge_color=edge_colors, node_size=10, node_color='black')
 plt.title('Graph Representation of a simple program', size=15)
 plt.show()
+
+
